d32e835e2f4e5a3faca830bea15678980ab793b1
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2024-02-07 12:35:32 +0200

    header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import { Navigate } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import Home from 'pages/Home/Home';
-import Catalog from '../pages/Psychologists/Psychologists';
-
 import Favorites from 'pages/Favorites/Favorites';
+import Psychologists from '../pages/Psychologists/Psychologists';
 
 const App = () => {
 
@@ -12,7 +11,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="catalog" element={<Catalog />} />
+        <Route path="psychologists" element={<Psychologists />} />
         <Route path="favorites" element={<Favorites />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Route>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,49 @@
 import React from 'react';
 import css from './Header.module.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const currentPath = useLocation().pathname;
+  const isLoggedIn = true;
+
   return (
     <div className={css.container}>
       <div className={css.header}>
-        <NavLink to="/" className={css.logoLink}>
-          <h1>psychologists.services</h1>
-        </NavLink>
+        <div className={css.headerPart}>
+          <NavLink to="/" className={css.logoLink}>
+            <h1 className={css.logoLinkh1}>
+              <span>psychologists.</span>services
+            </h1>
+          </NavLink>
+
+          <div className={css.navlinks}>
+            <NavLink to="/" className={currentPath === '/' ? 'active' : ''}>
+              <h2 className={css.navlinksH2}>Home</h2>
+            </NavLink>
+            <NavLink
+              to="/psychologists"
+              className={currentPath === '/psychologists' ? 'active' : ''}
+            >
+              <h2 className={css.navlinksH2}>Psychologists</h2>
+            </NavLink>
+            {isLoggedIn ? (
+              <NavLink
+                to="/Favorites"
+                className={currentPath === '/favorites' ? 'active' : ''}
+              >
+                <h2 className={css.navlinksH2}>Favorites</h2>
+              </NavLink>
+            ) : null}
+          </div>
+        </div>
+        {isLoggedIn ? (
+          <h1>logged in</h1>
+        ) : (
+          <div className={css.HeaderButtonsPart}>
+            <button className={css.HeaderButtonsPartLogIn}>Log in</button>
+            <button className={css.HeaderButtonsPartRegistration}>Registration</button>
+          </div>
+        )}
       </div>
     </div>
   );
