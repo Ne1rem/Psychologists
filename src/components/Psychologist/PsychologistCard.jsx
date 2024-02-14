8b5ffd33fa030a89ed654913fb647681ff9b75d3
Anatoliy Danilyuk <anatolydanilyuk2002@gmail.com>
2024-02-14 14:20:14 +0200

    connect database

--- src/components/Psychologist/PsychologistCard.jsx
+++ src/components/Psychologist/PsychologistCard.jsx
@@ -1,38 +1,115 @@
-import { AvatarWrap, DescriptionText, DescriptionWrap, HeadDescriptionWrap, PsyText, PsycWrap, PsychologistName, RatingPriceBlock, RatingPriceText, RatingPriceTextSpan, ReadMoreButton, SkillsBlock, SkillsBlockText, SkillsSpan, SvgHeart, SvgStar } from "./PsychologistCard.styled"
+import {
+  AvatarWrap,
+  DescriptionText,
+  DescriptionWrap,
+  HeadDescriptionWrap,
+  PsyText,
+  PsycWrap,
+  PsychologistName,
+  RatingPriceBlock,
+  RatingPriceText,
+  RatingPriceTextSpan,
+  ReadMoreButton,
+  SkillsBlock,
+  SkillsBlockText,
+  SkillsSpan,
+  SvgHeart,
+  SvgStar,
+} from './PsychologistCard.styled';
 import icons from '../../img/icons.svg';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { onValue } from 'firebase/database';
+import { database } from '../../firebase.js';
 
 const PsychologistCard = () => {
+  const filter = useContext(createContext(null));
+
+  const [ psychologists, setPsychologists] = useState([ ])
+  
+  useEffect(() => {
+    if (!filter) return;
+
+    if (filter.firstFilter) {
+      onValue(database, snapshot => {
+        let list;
+        if (filter.filter === 'price_per_hour') {
+          list = snapshot
+            .val()
+            .filter(item => item[filter.filter] <= filter.value);
+        } else {
+          list = snapshot
+            .val()
+            .filter(item => item[filter.filter].includes(filter.value));
+        }
+        setPsychologists(list);
+        console.log(list)
+      });
+    } 
+  }, 
+  [filter]);
+  
+  
+  useEffect(()=>{
+       onValue(database, (snapshot)=>{
+          const currentData = snapshot.val();
+          
+          setPsychologists(currentData)
+
+      })
+  } ,[ ])
+
+  console.log(psychologists);
+
   return (
     <PsycWrap>
-        <AvatarWrap></AvatarWrap>  
-        <DescriptionWrap>
-           <HeadDescriptionWrap>
-            <PsyText>Psychologist</PsyText>
-            <RatingPriceBlock>
-              <SvgStar>
-                <use href={`${icons}#icon-star`}></use>
-              </SvgStar>
-              <RatingPriceText>Rating: 4.7</RatingPriceText>
-              <RatingPriceText>Price / 1 hour: <RatingPriceTextSpan>180$</RatingPriceTextSpan> </RatingPriceText>
-            </RatingPriceBlock>
-            <SvgHeart>
-                <use href={`${icons}#icon-heart`}></use>
-              </SvgHeart>
-            </HeadDescriptionWrap> 
-            <PsychologistName>Dr. Mark Thompson</PsychologistName>
-            <SkillsBlock>
-              <SkillsBlockText><SkillsSpan>Experience: </SkillsSpan> 20 years</SkillsBlockText>
-              <SkillsBlockText><SkillsSpan>License: </SkillsSpan> Licensed Psychologist (License #54321)</SkillsBlockText>
-              <SkillsBlockText><SkillsSpan>Specialization: </SkillsSpan>  Relationship Counseling</SkillsBlockText>
-              <SkillsBlockText><SkillsSpan>Initial_consultation: </SkillsSpan> Free 60-minute initial consultation</SkillsBlockText>
-            </SkillsBlock>
-            <DescriptionText>
-            Dr. Mark Thompson is a highly experienced and licensed psychologist specializing in Relationship Counseling. With 20 years of practice, he has helped individuals navigate and improve their relationships, leading to better well-being and personal growth. Dr. Thompson is known for his expertise and ability to provide invaluable insights to his clients. His approach to therapy is tailored to each individual's unique needs, ensuring a supportive and effective counseling experience.
-            </DescriptionText>
-            <ReadMoreButton type="button">Read more</ReadMoreButton>
-        </DescriptionWrap>    
+      <AvatarWrap></AvatarWrap>
+      <DescriptionWrap>
+        <HeadDescriptionWrap>
+          <PsyText>Psychologist</PsyText>
+          <RatingPriceBlock>
+            <SvgStar>
+              <use href={`${icons}#icon-star`}></use>
+            </SvgStar>
+            <RatingPriceText>Rating: 4.7</RatingPriceText>
+            <RatingPriceText>
+              Price / 1 hour: <RatingPriceTextSpan>180$</RatingPriceTextSpan>{' '}
+            </RatingPriceText>
+          </RatingPriceBlock>
+          <SvgHeart>
+            <use href={`${icons}#icon-heart`}></use>
+          </SvgHeart>
+        </HeadDescriptionWrap>
+        <PsychologistName>Dr. Mark Thompson</PsychologistName>
+        <SkillsBlock>
+          <SkillsBlockText>
+            <SkillsSpan>Experience: </SkillsSpan> 20 years
+          </SkillsBlockText>
+          <SkillsBlockText>
+            <SkillsSpan>License: </SkillsSpan> Licensed Psychologist (License
+            #54321)
+          </SkillsBlockText>
+          <SkillsBlockText>
+            <SkillsSpan>Specialization: </SkillsSpan> Relationship Counseling
+          </SkillsBlockText>
+          <SkillsBlockText>
+            <SkillsSpan>Initial_consultation: </SkillsSpan> Free 60-minute
+            initial consultation
+          </SkillsBlockText>
+        </SkillsBlock>
+        <DescriptionText>
+          Dr. Mark Thompson is a highly experienced and licensed psychologist
+          specializing in Relationship Counseling. With 20 years of practice, he
+          has helped individuals navigate and improve their relationships,
+          leading to better well-being and personal growth. Dr. Thompson is
+          known for his expertise and ability to provide invaluable insights to
+          his clients. His approach to therapy is tailored to each individual's
+          unique needs, ensuring a supportive and effective counseling
+          experience.
+        </DescriptionText>
+        <ReadMoreButton type="button">Read more</ReadMoreButton>
+      </DescriptionWrap>
     </PsycWrap>
-  )
-}
+  );
+};
 
-export default PsychologistCard
+export default PsychologistCard;
--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA4SSYS6EVg8wWpcMAUixCwPdcis2yjfKc",
-    authDomain: "psychologists-8a8db.firebaseapp.com",
-    projectId: "psychologists-8a8db",
-    storageBucket: "psychologists-8a8db.appspot.com",
-    messagingSenderId: "769656068762",
-    appId: "1:769656068762:web:d6607f0362c9c72ee71d23"
-  };
+  apiKey: "AIzaSyA4SSYS6EVg8wWpcMAUixCwPdcis2yjfKc",
+  authDomain: "psychologists-8a8db.firebaseapp.com",
+  databaseURL: "https://psychologists-8a8db-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "psychologists-8a8db",
+  storageBucket: "psychologists-8a8db.appspot.com",
+  messagingSenderId: "769656068762",
+  appId: "1:769656068762:web:d6607f0362c9c72ee71d23"
+};
 
 export const app = initializeApp(firebaseConfig);
 export const database = ref(getDatabase(app));
